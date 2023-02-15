5ef02f74a0c645178631fc1b39416ab20ee1ecfa
Jules Casteran <jcasteran@scaleway.com>
2023-02-15 17:25:19 +0100

    add output prints in setup script

--- setup.sh
+++ setup.sh
@@ -4,12 +4,16 @@ KUBECONFIG_FILE=`terraform show -json | jq '.values.root_module.resources[] | se
 
 echo "${KUBECONFIG_FILE}" > ./kubeconfig
 
+echo "Installing waypoint server..."
+
 KUBECONFIG=./kubeconfig waypoint install -platform=kubernetes -accept-tos
 
 CONTAINER_NAMESPACE_ID_REGIONAL=`terraform show -json | jq '.values.root_module.resources[] | select(.address == "scaleway_container_namespace.namespace") | .values.id' -r`
 CONTAINER_NAMESPACE_ID="${CONTAINER_NAMESPACE_ID_REGIONAL#*/}"
 
 REGISTRY_ENDPOINT=`terraform show -json | jq '.values.root_module.resources[] | select(.address == "scaleway_container_namespace.namespace") | .values.registry_endpoint' -r`
+
+echo "Completing waypoint.hcl..."
 
 sed -i "s/{container-namespace}/${CONTAINER_NAMESPACE_ID}/g" waypoint.hcl
 sed -i "s#{registry-endpoint}#${REGISTRY_ENDPOINT}#g" waypoint.hcl
