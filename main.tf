38c9624ec7b454097a918f79929aa4721e100cef
Jules Casteran <jcasteran@scaleway.com>
2023-02-17 09:35:16 +0100

    reduce cluster pool size and use cheaper node type

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ resource "scaleway_k8s_cluster" "cluster" {
 resource "scaleway_k8s_pool" "pool" {
 	cluster_id = scaleway_k8s_cluster.cluster.id
 	name = "main-pool"
-	node_type = "PLAY2-MICRO"
-	size = 2
+	node_type = "DEV1-M"
+	size = 1
 }
 
 resource "scaleway_container_namespace" "namespace" {}
